d9e95fbbcb949f46df06097a44ebac8b54c2907b
grillzwitu <grillzwitu@gmail.com>
2021-01-25 23:33:50 +0100

    made some changes in my resume

--- profile-frontend/src/data/config.js
+++ profile-frontend/src/data/config.js
@@ -6,5 +6,5 @@ module.exports = {
   greetingDescription: "I'm Taremowei Appah and I am a Software Engineer",
   githubUrl: "https://github.com/grillzwitu",
   linkedinUrl: "www.linkedin.com/in/taremowei-appah-b816a51ba/",
-  cvLink: "https://drive.google.com/file/d/1gH-JH8sSrwjY2_rq7kE5JN5_n-2KQ2h0/view?usp=sharing",
+  cvLink: "https://drive.google.com/file/d/1p-I28hX7W5d13VKO8vOxl6A6Voo6368z/view?usp=sharing",
 };
